be8a12580a16e22bc854bf3276a2158f73c176fb
Kivu-123 <149676413+Kivu-123@users.noreply.github.com>
2023-11-03 11:39:29 +0200

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,14 @@
+public class Method
+{
+    public static int addnumbers(int num1 , int num2)
+    {
+        return num1+ num2;
+    }
+    public static void main(String[] args)
+    {
+        int number1 = 100;
+        int number2 = 17;
+        int result = addnumbers(number1, number2);
+        System.out.println(result);
+    }
+}
